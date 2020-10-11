79b393941fe26db27a9c97e9e6cb9e0f5c941cc1
Krystian Marcisz <simivar@gmail.com>
2020-10-12 01:14:23 +0200

    Fix translator namespace (#11)
    * Fix translator namespace
    
    * Bump version to 2.0.1

--- Translator/Translator.php
+++ Translator/Translator.php
@@ -2,9 +2,8 @@
 
 declare(strict_types=1);
 
-namespace Piwik\Plugins\PasswordPolicyEnforcer;
+namespace Piwik\Plugins\PasswordPolicyEnforcer\Translator;
 
-use Piwik\Plugins\PasswordPolicyEnforcer\Translator\TranslatorInterface;
 use Piwik\Translation\Translator as MatomoTranslator;
 
 final class Translator implements TranslatorInterface
--- config/config.php
+++ config/config.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 return [
-    'Piwik\Plugins\PasswordPolicyEnforcer\Translator' => DI\object()->constructor(
+    'Piwik\Plugins\PasswordPolicyEnforcer\Translator\Translator' => DI\autowire()->constructor(
         DI\get('Piwik\Translation\Translator')
     ),
-    'Piwik\Plugins\PasswordPolicyEnforcer\Validators\PasswordValidator' => DI\object()->constructor(
-        DI\get('Piwik\Plugins\PasswordPolicyEnforcer\Translator')
+    'Piwik\Plugins\PasswordPolicyEnforcer\Validators\PasswordValidator' => DI\autowire()->constructor(
+        DI\get('Piwik\Plugins\PasswordPolicyEnforcer\Translator\Translator')
     ),
 ];
